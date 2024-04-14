299bcf1c0b56f8640f268d2cea138b192ea33caf
Byungin KIM <andrewbkim.dev@gmail.com>
2024-04-14 13:49:03 +0900

    Add simple Counter Component with test

---
+++ src/app/components/counter/counter.component.spec.ts
@@ -0,0 +1,80 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { click, getRandomInt, getText, setFieldValue } from 'src/spec-helpers';
+import { CounterComponent } from './counter.component';
+
+describe('CounterComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [CounterComponent],
+    }).compileComponents();
+  });
+
+  function setup() {
+    const fixture = TestBed.createComponent(CounterComponent);
+    const component = fixture.componentInstance;
+    const startCount = getRandomInt();
+    component.startCount = startCount;
+
+    component.ngOnChanges();
+    fixture.detectChanges();
+
+    return { fixture, component };
+  }
+
+  it('should render component', () => {
+    // Arrange
+    // Act
+    const { component } = setup();
+    // Assert
+    expect(component).toBeTruthy();
+  });
+
+  it('should increment', () => {
+    // Arrange
+    const { fixture, component } = setup();
+    // Act
+    click(fixture, 'increment-button')
+    fixture.detectChanges();
+    // Assert
+    expectCount(fixture, component.startCount + 1, 'show incremented count');
+  });
+
+  it('should decrement', () => {
+    // Arrange
+    const { fixture, component } = setup();
+    // Act
+    click(fixture, 'decrement-button');
+    fixture.detectChanges();
+    // Assert
+    expectCount(fixture, component.startCount -1, 'show decremented count');
+  });
+
+  it('should reset counter', () => {
+    // Arrange
+    const { fixture } = setup();
+    const newCount = getRandomInt();
+    // Act
+    setFieldValue(fixture, 'reset-input', String(newCount));
+    click(fixture, 'reset-button');
+    fixture.detectChanges();
+    // Assert
+    expectCount(fixture, newCount, 'reset count');
+  });
+
+  it('should not reset counter if non-number', () => {
+    // Arrange
+    const { fixture, component } = setup();
+    const { startCount } = component;
+    // Act
+    const newCount = 'non-number';
+    setFieldValue(fixture, 'reset-input', String(newCount));
+    click(fixture, 'reset-button');
+    // Assert
+    expectCount(fixture, startCount, 'reset count');
+  });
+});
+
+function expectCount<T>(fixture: ComponentFixture<T>, a: number | string, context = '') {
+  const countText = getText(fixture, 'count');
+  expect(countText).withContext(context).toBe(String(a));
+}

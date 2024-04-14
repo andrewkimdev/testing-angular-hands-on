5cb44ca265d796ee7baf6cc6641ef785712ea2cf
Byungin KIM <andrewbkim.dev@gmail.com>
2024-04-14 13:45:46 +0900

    Add eslint config package

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.all,
+  ...tseslint.configs.all,
+];
